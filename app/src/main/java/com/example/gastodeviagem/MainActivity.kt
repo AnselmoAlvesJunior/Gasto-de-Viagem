e09f1798e8aef8c5b9b6bee77dc58a40062e2877
Anselmo Alves Junior <ansjunior@msn.com>
2020-08-18 11:16:30 -0300

    App de gasto com Kotlin

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcula.setOnClickListener(this)
     }
+
+    override fun onClick(p0: View?) {
+        val id= p0?.id
+
+        if (id==R.id.btnCalcula){
+            calcula()
+        }
+
+    }
+    private fun calcula(){
+        try{
+            val distancia = distanciaId.text.toString().toFloat()
+            val preco = precoId.text.toString().toFloat()
+            val autonomia = autonomiaId.text.toString().toFloat()
+
+            val totalValor = (distancia * preco) / autonomia
+            valorFinal.text = "R$ ${"%.2f".format(totalValor)}"
+        }catch (e:NumberFormatException ){
+            e.toString()
+            Toast.makeText(this,"Valores invalidos!",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
+
+
